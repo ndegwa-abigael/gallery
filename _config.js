d9dccfcdbf2c3ebc9bcea3e1aa62a62afe6f5f9a
root <root@IBSKE00511.impaxafrica.com>
2024-07-09 23:19:23 +0300

    Changed the config connection string

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://abigaelndegwa:Dev.Engineer@2024@cluster0.kb530wi.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://abigaelndegwa:Dev.Engineer@2024@cluster0.kb530wi.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://abigaelndegwa:Dev.Engineer@2024@cluster0.kb530wi.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://abigaelndegwa:Dev.Engineer@2024@gallery-cluster.f7l8mfe.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://abigaelndegwa:Dev.Engineer@2024@gallery-cluster.f7l8mfe.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://abigaelndegwa:Dev.Engineer@2024@gallery-cluster.f7l8mfe.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
+
+
+//mongodb+srv://abigaelndegwa:Dev.Engineer@2024@gallery-cluster.f7l8mfe.mongodb.net/
