af695b52a3fa56b49bc8ed2d837ce50d92ef9e88
root <root@IBSKE00511.impaxafrica.com>
2024-07-09 23:32:50 +0300

    Modified master config file

--- _config.js
+++ _config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var config = {}
 
 // Update to have your correct username and password
@@ -11,14 +10,3 @@ module.exports = config;
 
 
 //mongodb+srv://abigaelndegwa:Dev.Engineer@2024@gallery-cluster.f7l8mfe.mongodb.net/
-=======
-var config = {}
-
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
-module.exports = config;
->>>>>>> test
